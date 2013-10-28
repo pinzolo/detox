d115018e0352d014f70536408775b2cd2ed5faa8
pinzolo <pinzolo@gmail.com>
2013-10-28 16:37:13 +0900

    Refactor all.rb by totalizing that using meta programing

--- lib/detox/all.rb
+++ lib/detox/all.rb
@@ -1,61 +1,45 @@
 # coding: utf-8
+require "active_support/core_ext"
 require "detox/validity_broker"
-
-module Detox
-  class AllPresenceValidator < ActiveModel::EachValidator
-    include Detox::ArrayValidity
-
-    def validate_each(record, attribute, value)
-      values = convert_to_validatee(value, options.slice(*ArrayValidity::RESERVED_OPTIONS))
-      return if values.blank?
-
-      new_options = options.dup.except(*ArrayValidity::RESERVED_OPTIONS).merge(:attributes => [attribute])
-      validator = ActiveModel::Validations::PresenceValidator.new(new_options)
-      broker = ValidityBroker.new
-      validity = values.all? do |v|
-        broker.validatee = v
-        validator.validate(broker)
-        broker.valid?
-      end
-      unless validity
-        message = options[:message] || :all_presence
-        record.errors.add(attribute, message)
-      end
-    end
-  end
-
-  class AllFormatValidator < ActiveModel::EachValidator
-    include Detox::ArrayValidity
-
-    def validate_each(record, attribute, value)
-      values = convert_to_validatee(value, options.slice(*ArrayValidity::RESERVED_OPTIONS))
-      return if values.blank?
-
-      new_options = options.dup.except(*ArrayValidity::RESERVED_OPTIONS).merge(:attributes => [attribute])
-      validator = ActiveModel::Validations::FormatValidator.new(new_options)
-      broker = ValidityBroker.new
-      validity = values.all? do |v|
-        broker.validatee = v
-        validator.validate(broker)
-        broker.valid?
-      end
-      unless validity
-        message = options[:message] || :all_format
-        record.errors.add(attribute, message)
-      end
-    end
-  end
-end
 
 unless defined?(ActiveModel::Validations::AbsenceValidator)
   require "active_model/validations/absence"
 end
 
-#require "active_support/core_ext"
-#ignore_constants =  [:WithValidator]
-#consts = (ActiveModel::Validations.constants - ignore_constants).select do |c|
-#  const = "ActiveModel::Validations::#{c}".safe_constantize
-#  const.ancestors.include?(ActiveModel::EachValidator)
-#end
-#p consts
+ignore_constants =  [:WithValidator]
+defined_validators = Detox.constants
+src = ""
+ActiveModel::Validations.constants.each do |const|
+  next if ignore_constants.include?(const)
+  base_validator = "ActiveModel::Validations::#{const}".safe_constantize
+  next unless base_validator.ancestors.include?(ActiveModel::EachValidator)
+  next if defined_validators.include?("Detox::All#{const}".to_sym)
+
+  define_validator_src = <<-EOS
+  class All#{const} < ActiveModel::EachValidator
+    include Detox::ArrayValidity
+
+    def validate_each(record, attribute, value)
+      values = convert_to_validatee(value, options.slice(*ArrayValidity::RESERVED_OPTIONS))
+      return if values.blank?
+
+      new_options = options.dup.except(*ArrayValidity::RESERVED_OPTIONS).merge(:attributes => [attribute])
+      validator = ActiveModel::Validations::#{const}.new(new_options)
+      broker = ValidityBroker.new
+      validity = values.all? do |v|
+        broker.validatee = v
+        validator.validate(broker)
+        broker.valid?
+      end
+      unless validity
+        message = options[:message] || :all_#{const.to_s.underscore.gsub(/_validator\z/, "")}
+        record.errors.add(attribute, message)
+      end
+    end
+  end
+EOS
+  src << define_validator_src
+end
+
+Detox.module_eval(src)
 
