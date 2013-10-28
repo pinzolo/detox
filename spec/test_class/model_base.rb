130b15dfbbe0f66911395b57651b1fea1f6ce0c7
pinzolo <pinzolo@gmail.com>
2013-10-28 11:56:59 +0900

    Remove no using code

--- spec/test_class/model_base.rb
+++ spec/test_class/model_base.rb
@@ -1,22 +1,11 @@
 # coding: utf-8
 require "active_model"
 
-# Not use ActiveModel::Model for support Rails 3.2.x
+# Not use ActiveModel::Model for support ActiveModel 3.2.x
 class ModelBase
   extend  ActiveModel::Naming
   extend  ActiveModel::Translation
   include ActiveModel::Validations
   include ActiveModel::Conversion
 
-  def initialize(params={})
-    params.each do |attr, value|
-      self.public_send("#{attr}=", value)
-    end if params
-
-    super()
-  end
-
-  def persisted?
-    false
-  end
 end
